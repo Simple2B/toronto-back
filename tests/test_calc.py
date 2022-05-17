8e64474e2e93caba43586e52da67fe9429a32bc5
AlexAzalor <yablunovsky.a@gmail.com>
2022-05-17 15:49:47 +0300

    add test for functions

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,4 +1,5 @@
 from app.services import get_gas_cost, get_car_mileage
+from app.services.gas_calc import get_make_list, get_model_list, get_vehicle_year
 
 
 def test_gas_cost():
@@ -14,4 +15,23 @@ def test_gas_consumption():
     MODEL = "M8 Competition Coupe"
     YEAR = 2022
     res = get_car_mileage(model=MODEL, make=MAKE, year=YEAR)
+    assert res[0]
+    assert res[1]
+
+
+def test_make_list():
+    res = get_make_list()
     assert res
+
+
+def test_model_list():
+    MAKE = "Acura"
+    response = get_model_list(make=MAKE)
+    assert response
+
+
+def test_year_list():
+    MAKE = "Ford"
+    MODEL = "Transit Connect Van FWD"
+    response = get_vehicle_year(make=MAKE, model=MODEL)
+    assert response
